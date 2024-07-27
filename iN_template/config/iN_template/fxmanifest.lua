89f21cf61ef51fe6850edf08548452681c0fc3b8
b0tha <80119676+b0tha@users.noreply.github.com>
2024-07-27 09:28:29 +0200

    config.lua
    Added Config initialisation.

---
+++ iN_template/config/iN_template/fxmanifest.lua
@@ -0,0 +1,28 @@
+------------------------------
+-- Thank you for your support!
+------------------------------
+
+fx_version  'cerulean'
+game        'gta5'
+lua54       'yes'
+
+author      'imagiNix Studios'
+description ''
+version     '1.0.0'
+
+shared_scripts {
+    'config/*',
+    'shared/*'
+}
+
+server_scripts {
+    'server/*'
+}
+
+client_scripts  {
+    'client/*'
+}
+
+escrow_ignore {
+    'config.lua'
+}
